8635b0b8847cd3ee2618414a2f7c28e86aef2bc9
Todd Smith <tbsmith@fullsail.com>
2013-07-03 21:07:21 -0400

    Finished conditional script

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -2,4 +2,20 @@
 // 7-3-13
 // Conditionals
 //
-//
+
+// Ask for amount remaining in budget
+var remaining = Number(prompt("How much is left in the entertainmnet budget?", 30));
+
+// Print out the amount
+console.log("You typed in " + remaining);
+
+if (remaining >= 30) {
+    // there's enough
+    console.log("We can go!");
+    document.write("We can go!");
+} else {
+    // not enough
+    console.log("We cannot go.");
+    document.write("We cannot go.");
+}
+
